42cc6860da9df4e1097c3e0db147fb7a846d330b
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-03 10:30:11 -0500

    bonus commit

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,6 +1,19 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @search_text = ""
+    powers_arr = Power.all.map { |p| p.name.downcase }
+
+    # bonus: handle search queries on index page
+    if params[:search] && powers_arr.include?(params[:search].strip.downcase)
+      @power = Power.all.select { |p| p.name.downcase == params[:search].strip.downcase }[0]
+      @heroines = Heroine.all.select { |h| h.power_id == @power.id }
+      @search_text = params[:search]
+    elsif params[:search] && (params[:search].length > 0)
+      @error = "Power not found."
+      @heroines = Heroine.all
+    else
+      @heroines = Heroine.all
+    end
   end
 
   def show
