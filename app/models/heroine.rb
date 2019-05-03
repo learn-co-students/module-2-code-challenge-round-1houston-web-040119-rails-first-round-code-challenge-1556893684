315133665d6ddec8a25222362b48cf85245ae5ba
kreestoff <christopher.l.mcintosh@outlook.com>
2019-05-03 10:16:18 -0500

    done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,8 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :name, presence: true
+  validates :super_name, presence: true
+  validates :super_name, uniqueness: true
+  validates :power_id, presence: true
 end
