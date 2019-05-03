431377938a212b46a17d57b3a53d9cc0ec362813
PYN <yxp7531540@gmail.com>
2019-05-03 09:50:26 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show 
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new 
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      flash[:error] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+
+
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
