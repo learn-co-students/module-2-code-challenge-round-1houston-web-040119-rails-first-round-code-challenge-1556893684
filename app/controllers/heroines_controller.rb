297449a04cebd9cda8b3293471304888fc0d9f94
EC2 Default User <ec2-user@ip-172-31-33-230.us-east-2.compute.internal>
2019-05-03 15:00:47 +0000

    done without bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,35 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+  
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+  
+  def new
+    @heroine = Heroine.new
+  end
+  
+  def create
+    
+    @heroine = Heroine.new(heroine_params)
+    
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      flash[:error] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+  
+  
+  
+  private
+  
+  def heroine_params
+    params.require(:heroine).permit(:name, :power_id, :super_name)
+  end
+  
+  
 end
